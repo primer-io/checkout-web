9bb048da1ea3275a99fd7d7fa559efba383f4431
Andre Rabello <andre@primer.io>
2024-01-25 10:10:32 +0000

    chore: format

--- examples/features/cobadged/src/api/const.ts
+++ examples/features/cobadged/src/api/const.ts
@@ -1,9 +1,9 @@
-export const primerHeaders = {
+export const primerHeaders: HeadersInit = {
   accept: 'application/json',
   'content-type': 'application/json',
   'X-API-KEY': import.meta.env.API_KEY,
   'X-API-VERSION': '2.2',
-  "Legacy-Workflows": false
+  'Legacy-Workflows': 'false',
 };
 
 export const primerApiUrl = 'https://api.staging.primer.io';
