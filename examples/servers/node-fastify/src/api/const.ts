fca3d20b61c1e6ff7aa43d5cb976415a88e1f989
Aladin Taleb <aladin.taleb@gmail.com>
2024-01-25 17:24:50 +0100

    chore: clean examples folder

--- examples/servers/node-fastify/src/api/const.ts
+++
@@ -1,8 +0,0 @@
-export const primerHeaders = {
-  accept: 'application/json',
-  'content-type': 'application/json',
-  'X-API-KEY': process.env.API_KEY,
-  'X-API-VERSION': '2.2',
-};
-
-export const primerApiUrl = 'https://api.sandbox.primer.io';
