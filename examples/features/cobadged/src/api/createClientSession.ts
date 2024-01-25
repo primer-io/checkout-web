df714b2b93f65ae2e3093d9eb3d07440f7802d5e
Aladin Taleb <aladin.taleb@gmail.com>
2024-01-25 13:52:28 +0100

    feat: add data to client session (#2)

--- examples/features/cobadged/src/api/createClientSession.ts
+++ examples/features/cobadged/src/api/createClientSession.ts
@@ -11,6 +11,27 @@ export function createClientSession() {
       order: {
         countryCode: 'GB',
       },
+
+      // emailAddress and billingAddress are required for 3DS
+      customer: {
+        emailAddress: 'test@test.com',
+        mobileNumber: '+6588889999',
+        firstName: 'John',
+        lastName: 'Smith',
+        billingAddress: {
+          firstName: 'John',
+          lastName: 'Smith',
+          postalCode: 'CB94BQ',
+          addressLine1: '47A',
+          countryCode: 'CL',
+          city: 'Cambridge',
+          state: 'Cambridgeshire',
+        },
+      },
+
+      paymentMethod: {
+        orderedAllowedCardNetworks: ['CARTES_BANCAIRES', 'VISA', 'MASTERCARD'],
+      },
     },
     primerHeaders,
   );
